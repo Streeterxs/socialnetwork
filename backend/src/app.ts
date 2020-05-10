5ead9da3d4ac4974353e54678d871edbc3644827
Streeterxs <afonso_mg3@hotmail.com>
2020-05-10 16:14:01 -0300

    Social Network - Backend: Installing and enabling Cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import path from 'path';
 import { Schema } from './schema/Schema';
+import cors from 'cors';
 
 console.log('olá');
 console.log(path.resolve(__dirname, '/./../.env'));
@@ -13,6 +14,7 @@ console.log(process.env.MONGODB_URL);
 
 mongoose.connect(process.env.MONGODB_URL, {useNewUrlParser: true, useUnifiedTopology: true});
 const app = Express();
+app.use(cors());
 app.use('/graphql', graphqlHTTP({
     schema: Schema,
     graphiql: true

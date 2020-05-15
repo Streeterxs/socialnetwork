db9d9d89897b8c2e9b997fc0240d7a1724e53e45
Streeterxs <afonso_mg3@hotmail.com>
2020-05-14 21:57:14 -0300

    Social Network - Frontend: LoginForm fixes.

--- frontend/src/Pages/LoginPage/Components/loginForm.tsx
+++ frontend/src/Pages/LoginPage/Components/loginForm.tsx
@@ -16,7 +16,7 @@ const loginForm = ({emailChange, passwordChange, formSubmit}:
                     </label>
                 </div>
                 <div className="md:w-2/3">
-                    <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" onChange={(event) => emailChange(event.target.value)} id="inline-full-name" type="text" value="Jane Doe"/>
+                    <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" onChange={(event) => emailChange(event.target.value)} id="inline-full-name" type="text" placeholder="Jane Doe"/>
                 </div>
             </div>
             <div className="md:flex md:items-center mb-6">
@@ -41,7 +41,7 @@ const loginForm = ({emailChange, passwordChange, formSubmit}:
             <div className="md:flex md:items-center">
                 <div className="md:w-1/3"></div>
                 <div className="md:w-2/3">
-                    <button className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+                    <button className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
                         Sign Up
                     </button>
                 </div>

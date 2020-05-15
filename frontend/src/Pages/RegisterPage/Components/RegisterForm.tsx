73212b7d4c1b81588ae4905782f8c97aedbc6809
Streeterxs <afonso_mg3@hotmail.com>
2020-05-14 21:56:44 -0300

    Social Network - Frontend: Create user feature and mutation query.

--- frontend/src/Pages/RegisterPage/Components/RegisterForm.tsx
+++ frontend/src/Pages/RegisterPage/Components/RegisterForm.tsx
@@ -1,9 +1,53 @@
 import React from 'react';
 
-const RegisterForm = () => (
-    <div>
-        Register Form!
-    </div>
+const RegisterForm = ({emailChange, passwordChange, nameChange, formSubmit}: 
+    {
+        emailChange: (email: string) => void,
+        passwordChange: (password: string) => void,
+        nameChange: (name: string) => void,
+        formSubmit: (event: React.FormEvent<HTMLFormElement>) => void
+    }
+    ) => (
+    <form className="w-full max-w-sm" onSubmit={(event) => formSubmit(event)}>
+        <div className="md:flex md:items-center mb-6">
+            <div className="md:w-1/3">
+                <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="inline-full-name">
+                    Name
+                </label>
+            </div>
+            <div className="md:w-2/3">
+                <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" onChange={(event) => nameChange(event.target.value)} id="inline-full-name" type="text" placeholder="Jane Doe"/>
+            </div>
+        </div>
+        <div className="md:flex md:items-center mb-6">
+            <div className="md:w-1/3">
+                <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="inline-email">
+                    Email
+                </label>
+            </div>
+            <div className="md:w-2/3">
+                <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" onChange={(event) => emailChange(event.target.value)} id="inline-email" type="text" placeholder="jane_doe@gmail.com"/>
+            </div>
+        </div>
+        <div className="md:flex md:items-center mb-6">
+            <div className="md:w-1/3">
+                <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="inline-username">
+                    Password
+                </label>
+            </div>
+            <div className="md:w-2/3">
+                <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-username"  onChange={(event) => passwordChange(event.target.value)} type="password" placeholder="******************"/>
+            </div>
+        </div>
+        <div className="md:flex md:items-center">
+            <div className="md:w-1/3"></div>
+            <div className="md:w-2/3">
+                <button className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
+                    Register
+                </button>
+            </div>
+        </div>
+    </form>
 );
 
 export default RegisterForm;
--- frontend/src/Pages/RegisterPage/RegisterPage.tsx
+++ frontend/src/Pages/RegisterPage/RegisterPage.tsx
@@ -1,9 +1,56 @@
 import React from 'react';
+import graphql from 'babel-plugin-relay/macro';
 
 import { RegisterForm } from './Components';
+import { commitMutation } from 'react-relay';
+import environment from 'src/relay/environment';
 
-const RegisterPage = () => (
-    <RegisterForm/>
-);
+const registerMutation = graphql`
+  mutation RegisterPageLoginMutation($name: String!, $email: String!, $password: String!) {
+    CreateUser(input: {name: $name, email: $email, password: $password, clientMutationId: "1"}) {
+      user {
+        name,
+        email
+      }
+    }
+  }
+`;
+
+const RegisterPage = () => {
+    let name = '';
+    let email = '';
+    let password = '';
+    const handleFormSubmition = (event: React.FormEvent<HTMLFormElement>) => {
+        console.log(name);
+        console.log(email);
+        console.log(password);
+        event.preventDefault();
+        const variables = {
+            name,
+            email,
+            password
+        }
+        const disposable = commitMutation(
+            environment,
+            {
+                mutation: registerMutation,
+                variables,
+                onCompleted: (response: any, errors) => {
+                    console.log(response);
+                    console.log(errors);
+                },
+                onError: console.log
+            }
+            
+        );
+    }
+    return (
+        <RegisterForm
+            nameChange={(nameReturned) => {name = nameReturned}}
+            emailChange={(emailReturned) => {email = emailReturned}}
+            passwordChange={(passwordReturned) => {password = passwordReturned}}
+            formSubmit={handleFormSubmition}/>
+    );
+};
 
 export default RegisterPage;
